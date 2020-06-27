efac8e0ea174e463b2a05a59d570d69a3f7f93bd
Douglas Anderson <hockeybuggy@gmail.com>
2020-06-27 17:21:15 -0400

    Add tests for every blog post.
    This is gonna make the tests take longer to run, but now it will load
    every blog post and check that there are no errors and that they all
    have titles.
    
    Closes: #66

--- e2e_tests/blog.test.js
+++ e2e_tests/blog.test.js
@@ -1,8 +1,80 @@
 const puppeteer = require("puppeteer");
 
-const { BLOG_PAGE } = require("./pages");
+const { BASE_URL, BLOG_PAGE } = require("./pages");
 
 const iPhone = puppeteer.devices["iPhone 6"];
+
+// It seems a litte weird to manage this list of blog posts here in the tests.
+// This is done in order to use `describe.each` and ensure that each of the
+// blog posts load.
+let expectedBlogPosts = [
+  {
+    postDate: "2020-05-10",
+    postTitle: "Switching to Gatsby",
+    postPathName: "/blog/2020/05/switching-to-gatsby",
+  },
+  {
+    postDate: "2020-01-18",
+    postTitle: "Experimenting with Protobufs generated types in Rust",
+    postPathName:
+      "/blog/2020/01/experimenting-with-protobufs-generated-types-in-rust",
+  },
+  {
+    postDate: "2019-09-08",
+    postTitle: "Migrated to Hugo Static Site generator",
+    postPathName: "/blog/2019/09/migrated-to-hugo",
+  },
+  {
+    postDate: "2016-08-26",
+    postTitle: "Relative line numbers and you",
+    postPathName: "/blog/2016/08/relative-line-numbers-and-you",
+  },
+  {
+    postDate: "2016-01-18",
+    postTitle: "Vim splitting shortcuts",
+    postPathName: "/blog/2016/01/vim-splitting-shortcuts",
+  },
+  {
+    postDate: "2015-03-18",
+    postTitle: "Bashing long Python commands down to size",
+    postPathName: "/blog/2015/03/long-python-commands-down-to-size",
+  },
+  {
+    postDate: "2014-10-03",
+    postTitle: "What's the deal with the name hockeybuggy?",
+    postPathName: "/blog/2014/10/whats-the-deal-with-the-name-hockeybuggy",
+  },
+  {
+    postDate: "2014-09-22",
+    postTitle: "State of the Dotfiles",
+    postPathName: "/blog/2014/09/state-of-the-dotfiles",
+  },
+  {
+    postDate: "2014-06-23",
+    postTitle: "Adding Semantics",
+    postPathName: "/blog/2014/06/adding-semantics",
+  },
+  {
+    postDate: "2013-09-11",
+    postTitle: "Migrated to Jekyll",
+    postPathName: "/blog/2013/09/migrated-to-jekyll",
+  },
+  {
+    postDate: "2013-03-25",
+    postTitle: "Moving beyond word-wise motions",
+    postPathName: "/blog/2013/03/moving-beymond-word-wise-motions",
+  },
+  {
+    postDate: "2013-01-31",
+    postTitle: "A new a stylish look around the site. Thanks to sass",
+    postPathName: "/blog/2013/01/adding-sass-files",
+  },
+  {
+    postDate: "2012-12-02",
+    postTitle: "Taking this Pelican site live",
+    postPathName: "/blog/2012/12/taking-this-pelican-site-live",
+  },
+];
 
 describe("/blog (Blog index Page)", () => {
   const loadPage = async () => {
@@ -32,23 +104,54 @@ describe("/blog (Blog index Page)", () => {
     });
   });
 
-  it("should have a title", async () => {
-    await loadPage();
-
-    expect(await page.title()).toEqual(BLOG_PAGE.expected.title);
-  });
-
   it("should have links to many blog posts", async () => {
     await loadPage();
 
     const blogPosts = await page.$$eval("article", (articles) =>
       articles.map((article) => {
         const postTitle = article.querySelector("h3").textContent;
-        const postUrl = article.querySelector("a").href;
+        const postUrl = new URL(article.querySelector("a").href);
+        const postPathName = postUrl.pathname;
         const postDate = article.querySelector("time").dateTime;
-        return { postTitle, postUrl, postDate };
+        return { postTitle, postPathName, postDate };
       })
     );
-    expect(blogPosts).toMatchSnapshot();
+    expect(blogPosts).toEqual(expectedBlogPosts);
+  });
+});
+
+const blogPosts = expectedBlogPosts.map((blogPost) => [
+  blogPost.postPathName,
+  blogPost.postTitle,
+]);
+
+describe.each(blogPosts)("%s", (pathName, title) => {
+  const loadPage = async () => {
+    await page.emulate(iPhone);
+    const fullUrl = `${BASE_URL}${pathName}/`;
+    const response = await page.goto(fullUrl, {
+      waitUntil: "networkidle2",
+    });
+    expect(response.ok());
+    expect(page.url()).toEqual(fullUrl);
+  };
+
+  it("should have a title", async () => {
+    await loadPage();
+
+    expect(await page.title()).toEqual(`${title} | hockeybuggy.com`);
+  });
+
+  it("should load without error", async () => {
+    const errors = [];
+    page.on("console", (msg) => {
+      if (msg.type() === "error") {
+        errors.push({ errorMessage: msg.text() });
+      }
+    });
+
+    await loadPage();
+
+    expect(errors).toEqual([]);
   });
 });
