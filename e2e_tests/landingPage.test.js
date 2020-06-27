773f3d1aebe14889ac7398f3a03aa6cf365cbdb9
Douglas Anderson <hockeybuggy@gmail.com>
2020-06-27 16:55:24 -0400

    Write tests for the blog list page.
    The end to end tests for this project existed, but weren't crazy
    amazing. This commit starts the process of making them better.
    
    It does so by asserting that no errors are logged to the console as well
    as a snapshot test about what blog posts exist.

--- e2e_tests/landingPage.test.js
+++ e2e_tests/landingPage.test.js
@@ -5,7 +5,7 @@ const { LANDING_PAGE } = require("./pages");
 const iPhone = puppeteer.devices["iPhone 6"];
 
 describe("/ (Landing Page)", () => {
-  beforeAll(async () => {
+  const loadPage = async () => {
     await page.emulate(iPhone);
     const response = await page.goto(LANDING_PAGE.url, {
       waitUntil: "networkidle2",
@@ -13,17 +13,28 @@ describe("/ (Landing Page)", () => {
 
     expect(response.ok());
     expect(page.url()).toEqual(LANDING_PAGE.url);
-  });
+  };
 
   it("should load without error", async () => {
-    expect(await page.title()).toEqual(LANDING_PAGE.expected.title);
+    const errors = [];
+    page.on("console", (msg) => {
+      if (msg.type() === "error") {
+        errors.push({ errorMessage: msg.text() });
+      }
+    });
 
+    await loadPage();
+
+    expect(errors).toEqual([]);
+
+    // Not related to this specific assertion but while we're here:
     await page.screenshot({
       path: `e2e_tests/screenshots/landing_page.png`,
     });
   });
 
   it("should have a title", async () => {
+    await loadPage();
     expect(await page.title()).toEqual(LANDING_PAGE.expected.title);
   });
 });
