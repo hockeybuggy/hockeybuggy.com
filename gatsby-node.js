b5f1e303c2c0667c206380b3d8f79a1267b14bab
Douglas Anderson <hockeybuggy@gmail.com>
2020-07-03 12:04:17 -0400

    Switch to using `netlify` redirects.
    I tried to use a client side redirects created in Gatsby but it required
    loading the app before the redirect happened, which wasn't great.

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const _ = require(`lodash`);
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions;
+  const { createPage } = actions;
 
   const blogPostTemplate = path.resolve(`./src/templates/blogPost.tsx`);
   const tagTemplate = path.resolve("src/templates/tags.tsx");
@@ -52,28 +52,6 @@ exports.createPages = async ({ graphql, actions }) => {
       context: {
         slug: post.node.fields.slug,
       },
-    });
-  });
-
-  [
-    "/2020/05/switching-to-gatsby",
-    "/2020/01/experimenting-with-protobufs-generated-types-in-rust",
-    "/2019/09/migrated-to-hugo",
-    "/2016/08/relative-line-numbers-and-you",
-    "/2016/01/vim-splitting-shortcuts",
-    "/2015/03/long-python-commands-down-to-size",
-    "/2014/10/whats-the-deal-with-the-name-hockeybuggy",
-    "/2014/09/state-of-the-dotfiles",
-    "/2014/06/adding-semantics",
-    "/2013/09/migrated-to-jekyll",
-    "/2013/03/moving-beymond-word-wise-motions",
-    "/2013/01/adding-sass-files",
-    "/2012/12/taking-this-pelican-site-live",
-  ].map((path) => {
-    createRedirect({
-      fromPath: `/blog/${path}`,
-      toPath: `/blog/post/${path}`,
-      isPermanent: true,
     });
   });
 
