b6da86ee798c68bf41620aa75f387b02a04a24a8
Douglas Anderson <hockeybuggy@gmail.com>
2020-07-27 16:16:45 -0400

    Start adding some end to end tests.
    I would ideally like to have good test coverage for the website since it
    allows things like automatic dependency updates less risky. This commit
    adds some of the projects page.

---
+++ e2e_tests/projects.test.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-unused-vars */
+const { BASE_URL, PROJECTS_PAGE } = require("./pages");
+const { loadPage } = require("./utils");
+
+const expectedProjects = [
+  {
+    postTitle: "This website",
+    postPathName: "/project/this-site",
+  },
+];
+
+describe("/projects (Projects index Page)", () => {
+  it("should load without error", async () => {
+    const errors = [];
+    page.on("console", (msg) => {
+      if (msg.type() === "error") {
+        errors.push({ errorMessage: msg.text() });
+      }
+    });
+
+    await loadPage(page, PROJECTS_PAGE.url);
+
+    expect(errors).toEqual([]);
+
+    // Not related to this specific assertion but while we're here:
+    await page.screenshot({
+      path: `e2e_tests/screenshots/projects_index_page.png`,
+    });
+  });
+
+  it("should have links to many projects", async () => {
+    await loadPage(page, PROJECTS_PAGE.url);
+
+    const projects = await page.$$eval("article", (articles) =>
+      articles.map((article) => {
+        const postTitle = article.querySelector("h3").textContent;
+        const postUrl = new URL(article.querySelector("a").href);
+        const postPathName = postUrl.pathname;
+        return { postTitle, postPathName };
+      })
+    );
+    expect(projects).toEqual(expectedProjects);
+  });
+});
