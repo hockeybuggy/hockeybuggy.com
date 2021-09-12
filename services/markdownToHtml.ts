f43c815744d993abc6356f69607e52e3c8eda5ba
Douglas Anderson <hockeybuggy@gmail.com>
2021-09-12 19:52:43 -0300

    Continue trying to improve the markdown processing.

--- services/markdownToHtml.ts
+++ services/markdownToHtml.ts
@@ -2,8 +2,9 @@ import remark from "remark";
 import html from "remark-html";
 import gfm from "remark-gfm";
 import slug from "rehype-slug";
+import remarkRehype from "remark-rehype";
 import autolinkHeadings from "rehype-autolink-headings";
-import smartyPants from "remark-gfm";
+import stringify from "rehype-stringify";
 
 export default async function markdownToHtml(
   markdown: string
@@ -11,9 +12,10 @@ export default async function markdownToHtml(
   const result = await remark()
     .use(html)
     .use(gfm)
-    .use(smartyPants)
+    .use(remarkRehype)
     .use(slug)
     .use(autolinkHeadings)
+    .use(stringify)
     .process(markdown);
   return result.toString();
 }
