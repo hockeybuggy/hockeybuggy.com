b00e7ec2c3dbc2ff3cd365b29b455eb034e09d85
Douglas Anderson <hockeybuggy@gmail.com>
2021-12-26 14:17:30 -0400

    Update to use the `NEXT_PUBLIC_` prefix.
    It seems like the environment variable I was using is not being exposed
    correctly. I am wondering if it's related to this:
    https://nextjs.org/docs/basic-features/environment-variables#exposing-environment-variables-to-the-browser

--- services/gtag.ts
+++ services/gtag.ts
@@ -4,7 +4,7 @@ declare global {
   }
 }
 
-export const GA_TRACKING_ID = process.env.PUBLIC_GA_ID;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string): void => {
