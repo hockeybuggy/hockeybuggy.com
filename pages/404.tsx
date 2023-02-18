aca81af8d0eb64ed1b937275948abf5b7f4ba338
Douglas Anderson <hockeybuggy@gmail.com>
2023-02-18 18:29:17 -0400

    Update the 404 page to send a custom event
    This is based off of: https://plausible.io/docs/error-pages-tracking-404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,20 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { BaseLayout } from "../layouts";
 
-const NotFoundPage: React.FC = () => (
-  <BaseLayout>
-    <h1>PAGE NOT FOUND</h1>
-    <p>You just landed on a page that does not exist... </p>
-    <p>Sorry about that.</p>
-  </BaseLayout>
-);
+import { track } from "../services/tracking";
+
+const NotFoundPage = (): JSX.Element => {
+  useEffect(() => {
+    track("404", { props: { path: document.location.pathname } });
+  }, []);
+
+  return (
+    <BaseLayout>
+      <h1>PAGE NOT FOUND</h1>
+      <p>You just landed on a page that does not exist... </p>
+      <p>Sorry about that.</p>
+    </BaseLayout>
+  );
+};
 
 export default NotFoundPage;
