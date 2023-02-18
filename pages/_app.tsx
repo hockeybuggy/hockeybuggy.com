b2b7680874e46421b5386ae9ac3d2c5d16bc3c8f
Douglas Anderson <hockeybuggy@gmail.com>
2023-02-18 18:50:24 -0400

    Change how plausible is being loaded
    We appeared to have been looking the env var (because it's build time
    only and we were trying to use it as if it was runtime).

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,30 @@
 import type { AppProps } from "next/app";
 import Script from "next/script";
-import { isProduction } from "../services/buildContext";
+import {
+  setupContext,
+  isProduction,
+  isDeployPreview,
+} from "../services/buildContext";
 
 import "../styles/main.scss";
+
+setupContext(process.env.CONTEXT);
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      {isProduction() && (
-        <Script
-          defer
-          data-domain="hockeybuggy.com"
-          src="https://plausible.io/js/script.js"
-        ></Script>
+      {(isProduction() || isDeployPreview()) && (
+        <>
+          <Script
+            defer
+            data-domain="hockeybuggy.com"
+            src="https://plausible.io/js/script.js"
+          ></Script>
+          <Script id="plausible-custom">
+            {`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}
+          </Script>
+        </>
       )}
-      <Script id="plausible-custom">
-        {`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}
-      </Script>
 
       <Component {...pageProps} />
     </>
