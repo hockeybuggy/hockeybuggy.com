ce56667ee3dfd007292eb22454210179c2832bf4
Douglas Anderson <hockeybuggy@gmail.com>
2023-02-18 18:41:06 -0400

    Add an explicit fallback of "dev" for when `CONTEXT` isn't set.

--- services/buildContext.ts
+++ services/buildContext.ts
@@ -1,10 +1,11 @@
 // https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
-const context = process.env.CONTEXT as
+const context = (process.env.CONTEXT || "dev") as
   | "production"
   | "deploy-preview"
   | "branch-deploy"
   | "dev";
 
 export function isProduction(): boolean {
+  console.log({ context });
   return context === "production";
 }
