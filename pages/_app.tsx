b63aba39a739bc828ecafcd2f7d3ac7a69252028
Douglas Anderson <hockeybuggy@gmail.com>
2023-02-18 17:11:18 -0400

    Remove the previous page view workaround
    The Google analytics script didn't work super well with static sites.
    This commit removes the reminder of that logic.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,9 @@
-import { useEffect } from "react";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import Script from "next/script";
 
 import "../styles/main.scss";
 
-const pageview = (url: string) => {
-  console.log(url);
-};
-
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <>
       <Script
