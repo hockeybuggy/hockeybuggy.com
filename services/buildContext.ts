a7d57a26ac53507cbc6c7995cbd85f7b9da011ed
Douglas Anderson <hockeybuggy@gmail.com>
2023-02-18 18:50:24 -0400

    Change how plausible is being loaded
    We appeared to have been looking the env var (because it's build time
    only and we were trying to use it as if it was runtime).

--- services/buildContext.ts
+++ services/buildContext.ts
@@ -1,11 +1,17 @@
 // https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
-const context = (process.env.CONTEXT || "dev") as
-  | "production"
-  | "deploy-preview"
-  | "branch-deploy"
-  | "dev";
+
+type CONTEXT = "production" | "deploy-preview" | "branch-deploy" | "dev";
+
+let context: CONTEXT;
+
+export function setupContext(contextEnv: string | undefined): void {
+  context = (contextEnv || "dev") as CONTEXT;
+}
 
 export function isProduction(): boolean {
-  console.log({ context });
   return context === "production";
+}
+
+export function isDeployPreview(): boolean {
+  return context === "deploy-preview";
 }
