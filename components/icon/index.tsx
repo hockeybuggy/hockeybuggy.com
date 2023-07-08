c1c95fb6233c36cf83b7f273a15b90c8cfafe5a6
Douglas Anderson <hockeybuggy@gmail.com>
2023-07-08 16:43:08 -0300

    Change to using inline svgs for icons
    We were getting the icons via a fortawesome package. The newest version
    of this package was not installing cleanly and we were using very few of
    the icons.
    
    This commit changes to manually including the svgs as normal react
    components.

---
+++ components/icon/index.tsx
@@ -0,0 +1,99 @@
+import * as React from "react";
+
+import barsIcon from "./svgs/bars";
+import emailIcon from "./svgs/email";
+import rssIcon from "./svgs/rss";
+import calendarIcon from "./svgs/calendar";
+import tagIcon from "./svgs/tag";
+import folderIcon from "./svgs/folder";
+import linkIcon from "./svgs/link";
+import creativeCommonsIcon from "./svgs/creativeCommons";
+import githubIcon from "./svgs/github";
+import twitterIcon from "./svgs/twitter";
+
+enum IconNames {
+  Bars,
+  Calendar,
+  Email,
+  Folder,
+  Link,
+  RSS,
+  Tag,
+
+  GitHub,
+  Twitter,
+  CreativeCommons,
+}
+
+enum IconSizes {
+  Default,
+  Large,
+}
+
+// This function is used to guard against non-exhaustive icon name lookups.
+function exhasutiveCheck(_: never) {} // eslint-disable-line
+
+function iconSvgByName(name: IconNames): React.ReactElement {
+  switch (name) {
+    case IconNames.Bars:
+      return barsIcon();
+    case IconNames.Email:
+      return emailIcon();
+    case IconNames.RSS:
+      return rssIcon();
+    case IconNames.Calendar:
+      return calendarIcon();
+    case IconNames.Tag:
+      return tagIcon();
+    case IconNames.Folder:
+      return folderIcon();
+    case IconNames.Link:
+      return linkIcon();
+    case IconNames.CreativeCommons:
+      return creativeCommonsIcon();
+    case IconNames.GitHub:
+      return githubIcon();
+    case IconNames.Twitter:
+      return twitterIcon();
+  }
+  exhasutiveCheck(name);
+}
+
+function pxBySize(size: undefined | IconSizes): string {
+  switch (size) {
+    case undefined:
+    case IconSizes.Default:
+      return "16px";
+    case IconSizes.Large:
+      return "64px";
+  }
+  exhasutiveCheck(size);
+}
+
+const Icon = ({
+  name,
+  label,
+  size,
+  width,
+}: {
+  name: IconNames;
+  label: string;
+  size?: IconSizes;
+  width?: string;
+}): JSX.Element => (
+  <i
+    className="icon"
+    aria-label={label}
+    style={{
+      width: width || pxBySize(size),
+      height: width || pxBySize(size),
+      fill: "currentColor",
+    }}
+  >
+    {iconSvgByName(name)}
+  </i>
+);
+Icon.Names = IconNames;
+Icon.Sizes = IconSizes;
+
+export default Icon;
