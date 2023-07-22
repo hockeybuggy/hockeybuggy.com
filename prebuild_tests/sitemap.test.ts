beccb7be79d4e58700b666f8d3da3b64b67a2bd3
Douglas Anderson <hockeybuggy@gmail.com>
2023-07-22 19:49:26 -0300

    Get the sitemap tests to test for contents
    We previously had some tests that were asserting that the page existed,
    but not what was in it. This commit asserts that there are some
    reasonable minimums for the different groups of pages.
    
    As I am writing this it should be exhaustive.

--- prebuild_tests/sitemap.test.ts
+++ prebuild_tests/sitemap.test.ts
@@ -1,4 +1,5 @@
 import { open } from "node:fs/promises";
+import type { XMLDocument } from "libxmljs";
 import { parseXmlAsync } from "libxmljs";
 
 async function loadSitemap() {
@@ -9,6 +10,22 @@ async function loadSitemap() {
   return document;
 }
 
+function extractUrlsFromSitemap(sitemap: XMLDocument) {
+  const urlElements = sitemap.find("*");
+  const urls = urlElements.map((ele) => {
+    // const name = ele.find("*")[0].childNodes()[0].text();
+    const children = ele.find("*");
+    const name = children[0].childNodes()[0].text();
+    let lastmod = null;
+    if (children.length > 1) {
+      lastmod = children[1].childNodes()[0].text();
+    }
+    return { name, lastmod };
+  });
+
+  return urls;
+}
+
 describe("Sitemap", () => {
   it("should have a version and encoding", async () => {
     const subject = await loadSitemap();
@@ -16,31 +33,70 @@ describe("Sitemap", () => {
     expect(subject.encoding()).toEqual("UTF-8");
   });
 
-  it("should have a urlset that contains urls", async () => {
+  it("should have a schema listed", async () => {
     const subject = await loadSitemap();
-    // The root node (the `urlset`) has a reference to the sitemap schema
     const urlsetNode = subject.root()!;
     expect(urlsetNode!.namespace()!.href()).toEqual(
-      "http://www.sitemaps.org/schemas/sitemap/0.9"
+      "http://www.sitemaps.org/schemas/sitemap/0.9",
     );
-
-    expect(subject.childNodes().length).toBeGreaterThan(1);
-    // Here we assume that the first url is the home page
-    const exampleUrlNode = urlsetNode.get(".url");
-    console.log(exampleUrlNode);
-    // expect(exampleUrlNode.childNodes().length).toEqual(1);
-    // expect(exampleUrlNode.get("loc")!).toEqual("https://hockeybuggy.com");
-    expect(true).toBe(false);
   });
 
-  it.todo("should have pages for the blog");
-  // index page
-  // The blog posts have a lastmod date
-  // post pages
-  // tags pages
-  // categories pages
+  it("should have pages for the index", async () => {
+    const subject = await loadSitemap();
 
-  it.todo("should have pages for the project");
-  // index page
-  // project pages
+    const urls = extractUrlsFromSitemap(subject);
+    const urlNames = urls.map((url) => url.name);
+    // index page
+    expect(urlNames).toContain("https://hockeybuggy.com");
+  });
+
+  it("should have pages for the blog", async () => {
+    const subject = await loadSitemap();
+
+    const urls = extractUrlsFromSitemap(subject);
+    const urlNames = urls.map((url) => url.name);
+    // index page for the blog
+    expect(urlNames).toContain("https://hockeybuggy.com/blog");
+
+    const blogPostPattern = /\/blog\/post/;
+    const blogPosts = urls.filter((url) => blogPostPattern.test(url.name));
+    // There are post pages. 10 is an arbitary number.
+    expect(blogPosts.length).toBeGreaterThan(10);
+    // All the blog posts have a lastmod date
+    expect(
+      blogPosts.map((post) => post.lastmod).every((post) => post !== null),
+    ).toBe(true);
+
+    // tags pages
+    expect(urlNames).toContain("https://hockeybuggy.com/blog/tags");
+    const blogTagPattern = /\/blog\/tags/;
+    const blogTagPages = urls.filter((url) => blogTagPattern.test(url.name));
+    // There are tag pages. 10 is an arbitary number.
+    expect(blogTagPages.length).toBeGreaterThan(10);
+
+    // categories pages
+    expect(urlNames).toContain("https://hockeybuggy.com/blog/categories");
+    const blogCategoriesPattern = /\/blog\/categories/;
+    const blogCategoriesPages = urls.filter((url) =>
+      blogCategoriesPattern.test(url.name),
+    );
+    // There are category pages. 4 is an arbitary number.
+    expect(blogCategoriesPages.length).toBeGreaterThan(4);
+  });
+
+  it("should have pages for the project", async () => {
+    const subject = await loadSitemap();
+
+    const urls = extractUrlsFromSitemap(subject);
+    const urlNames = urls.map((url) => url.name);
+    // index page for the projects
+    expect(urlNames).toContain("https://hockeybuggy.com/projects");
+
+    const projectPagePattern = /\/project\/\w*/;
+    const projectPages = urls.filter((url) =>
+      projectPagePattern.test(url.name),
+    );
+    // There are post pages. 5 is an arbitary number.
+    expect(projectPages.length).toBeGreaterThan(5);
+  });
 });
