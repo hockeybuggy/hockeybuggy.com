cf4913d772ad3d3aee6137674553c8fb7b740431
Douglas Anderson <hockeybuggy@gmail.com>
2024-10-17 16:51:38 -0300

    Use libxml2-wasm
    Trying to use this alternative wasm based package to resolve the vuln.
    
    Closes: #2158

--- prebuild_tests/sitemap.test.ts
+++ prebuild_tests/sitemap.test.ts
@@ -1,24 +1,22 @@
-import { open } from "node:fs/promises";
-import type { XMLDocument } from "libxmljs";
-import { parseXmlAsync } from "libxmljs";
+import fs from "node:fs";
+import { XmlDocument } from "libxml2-wasm";
 
 async function loadSitemap() {
-  const file = await open("./public/sitemap.xml");
-
-  const document = parseXmlAsync(await file.readFile());
+  const document = XmlDocument.fromString(
+    fs.readFileSync("./public/sitemap.xml").toString()
+  );
 
   return document;
 }
 
-function extractUrlsFromSitemap(sitemap: XMLDocument) {
+function extractUrlsFromSitemap(sitemap: XmlDocument) {
   const urlElements = sitemap.find("*");
   const urls = urlElements.map((ele) => {
-    // const name = ele.find("*")[0].childNodes()[0].text();
     const children = ele.find("*");
-    const name = children[0].childNodes()[0].text();
+    const name = children[0].find("*")[0].content;
     let lastmod = null;
     if (children.length > 1) {
-      lastmod = children[1].childNodes()[0].text();
+      lastmod = children[1].find("*")[0].content;
     }
     return { name, lastmod };
   });
@@ -29,16 +27,20 @@ function extractUrlsFromSitemap(sitemap: XMLDocument) {
 describe("Sitemap", () => {
   it("should have a version and encoding", async () => {
     const subject = await loadSitemap();
-    expect(subject.version()).toEqual("1.0");
-    expect(subject.encoding()).toEqual("UTF-8");
+    expect(subject.get("version")).toEqual("1.0");
+    expect(subject.get("encoding")).toEqual("UTF-8");
+
+    subject.dispose();
   });
 
   it("should have a schema listed", async () => {
     const subject = await loadSitemap();
-    const urlsetNode = subject.root()!;
-    expect(urlsetNode!.namespace()!.href()).toEqual(
-      "http://www.sitemaps.org/schemas/sitemap/0.9",
+    const urlsetNode = subject.root!;
+    expect(urlsetNode!.namespaceUri).toEqual(
+      "http://www.sitemaps.org/schemas/sitemap/0.9"
     );
+
+    subject.dispose();
   });
 
   it("should have pages for the index", async () => {
@@ -48,6 +50,8 @@ describe("Sitemap", () => {
     const urlNames = urls.map((url) => url.name);
     // index page
     expect(urlNames).toContain("https://hockeybuggy.com");
+
+    subject.dispose();
   });
 
   it("should have pages for the blog", async () => {
@@ -64,7 +68,7 @@ describe("Sitemap", () => {
     expect(blogPosts.length).toBeGreaterThan(10);
     // All the blog posts have a lastmod date
     expect(
-      blogPosts.map((post) => post.lastmod).every((post) => post !== null),
+      blogPosts.map((post) => post.lastmod).every((post) => post !== null)
     ).toBe(true);
 
     // tags pages
@@ -78,10 +82,12 @@ describe("Sitemap", () => {
     expect(urlNames).toContain("https://hockeybuggy.com/blog/categories");
     const blogCategoriesPattern = /\/blog\/categories/;
     const blogCategoriesPages = urls.filter((url) =>
-      blogCategoriesPattern.test(url.name),
+      blogCategoriesPattern.test(url.name)
     );
     // There are category pages. 4 is an arbitary number.
     expect(blogCategoriesPages.length).toBeGreaterThan(4);
+
+    subject.dispose();
   });
 
   it("should have pages for the project", async () => {
@@ -94,9 +100,10 @@ describe("Sitemap", () => {
 
     const projectPagePattern = /\/project\/\w*/;
     const projectPages = urls.filter((url) =>
-      projectPagePattern.test(url.name),
+      projectPagePattern.test(url.name)
     );
     // There are post pages. 5 is an arbitary number.
     expect(projectPages.length).toBeGreaterThan(5);
+    subject.dispose();
   });
 });
